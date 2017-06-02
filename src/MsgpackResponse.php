1bba354476578801c46c503e8c1191464f60f8e9
sc0Vu <alk03073135@gmail.com>
2017-06-02 09:09:00 +0800

    Set content type when user not set Set content type when user not set.

--- src/MsgpackResponse.php
+++ src/MsgpackResponse.php
@@ -75,9 +75,7 @@ class MsgpackResponse extends BaseResponse
      */
     protected function update()
     {
-        // Only set the header when there is none or when it equals 'text/javascript' (from a previous update with callback)
-        // in order to not overwrite a custom definition.
-        if (!$this->headers->has('Content-Type') || 'text/javascript' === $this->headers->get('Content-Type')) {
+        if (!$this->headers->has('Content-Type')) {
             $this->headers->set('Content-Type', 'application/x-msgpack');
         }
         $hex = implode("", unpack("H*", $this->data));
